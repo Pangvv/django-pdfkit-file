725cced00188feca779ed0a383b1c727f20d92cb
jiaxin <edison7500@gmail.com>
2018-04-23 11:23:16 +0800

    support string list

--- render_pdf/render.py
+++ render_pdf/render.py
@@ -25,7 +25,7 @@ options = {
     'zoom': 3.0,
     'orientation': 'Portrait',
     'cache-dir': '/tmp/pdf',
-    # 'window-status': 'ready_to_print',
+    'window-status': 'ready_to_print',
     'javascript-delay': 1000,
     # 'disable-javascript': None,
     # 'custom-header' : [
@@ -35,28 +35,28 @@ options = {
 }
 
 
-def html2pdf(urls, **kwargs):
-    _urls = urls
-    out = pdfkit.from_url(_urls, False,
-                          options=options)
+# def html2pdf(urls, **kwargs):
+#     _urls = urls
+#     out = pdfkit.from_url(_urls, False,
+#                           options=options)
+#
+#     return out
 
-    return out
 
-
-def html2pdf_string(filename, strings, opt=options):
-    storage = get_storage_class()
-    s = storage()
-    _filename = filename
-    _strings = strings
-    _options = opt
-    out_pdf = pdfkit.from_string(_strings,
-                                 False,
-                                 options=_options)
-    output = StringIO()
-    output.write(out_pdf)
-    s.save(name=_filename, content=output)
-    output.close()
-    return _filename
+# def html2pdf_string(filename, strings, opt=options):
+#     storage = get_storage_class()
+#     s = storage()
+#     _filename = filename
+#     _strings = strings
+#     _options = opt
+#     out_pdf = pdfkit.from_string(_strings,
+#                                  False,
+#                                  options=_options)
+#     output = StringIO()
+#     output.write(out_pdf)
+#     s.save(name=_filename, content=output)
+#     output.close()
+#     return _filename
 
 
 class RenderPDF(object):
@@ -74,23 +74,19 @@ class RenderPDF(object):
         output.close()
         return _filename
 
-    def html2pdf_from_string(self, string, filename):
-        out_pdf = pdfkit.from_string(string, False, options=self.options)
+    def html2pdf_from_string(self, string_list, filename):
+
+        if type(string_list) is not list:
+            raise Exception("please input string list")
+
+        _string = ''.join(string_list)
+        out_pdf = pdfkit.from_string(_string, False, options=self.options)
         return self._save_to_file(content=out_pdf,
                                   filename=filename)
 
 
 if __name__ == "__main__":
-    # urls = list()
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5ab7830e6c856923a8c71ba5.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e40e5c4a59a7623e933d3.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e42d4c4a59a787c7388f7.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e352ac4a59a73a8852e54.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e36f7c4a59a73a8852e55.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e374cc4a59a7623e933cb.md")
-    # urls.append("http://v.coolsite360.com/documents/doc-021724019383/5a8e37a6c4a59a73a8852e56.md")
-    # print (urls)
-    # pdfkit.from_url(urls, "out.pdf", options=options)
-    # pdfkit.from_url(['https://www.epub360.com/', 'http://www.coolsite360.com/'],
-    #                 'out.pdf')
-    html2pdf_string('/tmp/test.pdf', strings=["<html><h1>测试</h1><html>"])
+    render = RenderPDF()
+
+
+
